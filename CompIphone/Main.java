96cff95781da0578467060c3462292d972679830
degoNDL <diegoclebson32@gmail.com>
2025-04-02 19:40:32 -0300

    desafio POO componentes iphone

---
+++ CompIphone/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        IPhone meuIphone = new IPhone();
+
+        // Testando o Reprodutor Musical
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        // Testando o Aparelho Telefônico
+        meuIphone.ligar("11999999999");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        // Testando o Navegador
+        meuIphone.exibirPagina("https://www.apple.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
